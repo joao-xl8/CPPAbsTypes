f43de27eba3561c7104bef25d2094787e3670040
joao_arnold.py <joaopaulo167@gmail.com>
2023-09-16 13:41:25 -0300

    Initial Commit

---
+++ AbstracTypes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AbstracTypes
+//
+//  Created by João Paulo Arnold Barros on 16/09/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
